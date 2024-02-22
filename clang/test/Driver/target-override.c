8ebedd8efe0a44fb88f2ddbbb41e278a3b30f800
zijunzhao <zijunzhao@google.com>
2024-02-22 01:11:35 +0000

    Revert "[Driver] Allow target override containing . in executable name"
    This reverts commit 0d7eba1aeed853798bd8012f786c305e83a97b67.
    
    Change-Id: I02783959a64d3aa0caaf9883f14b104b13115b58

--- clang/test/Driver/target-override.c
+++ clang/test/Driver/target-override.c
@@ -3,7 +3,6 @@
 
 // RUN: rm -rf %t && mkdir %t
 // RUN: ln -s %clang %t/i386-clang
-// RUN: ln -s %clang %t/x86_64-pc-freebsd13.1-clang
 
 // Check if invocation of "foo-clang" adds option "-target foo".
 //
@@ -14,7 +13,3 @@
 //
 // RUN: %t/i386-clang -c --target=x86_64 -### %s 2>&1 | FileCheck -check-prefix CHECK-TG2 %s
 // CHECK-TG2: Target: x86_64
-
-/// Check if invocation of "arch-vendor-osX.Y-clang" adds option "-target arch-vendor-osX.Y".
-// RUN: %t/x86_64-pc-freebsd13.1-clang -c -### %s 2>&1 | FileCheck -check-prefix CHECK-TG3 %s
-// CHECK-TG3: Target: x86_64-pc-freebsd13.1
